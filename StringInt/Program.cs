84d15a863765a3749bf744ab0fdf1f316d9a3297
Usuario <Usuario@DESKTOP-D7TBL2I>
2022-03-21 17:29:48 -0700

    Agregar archivos de proyecto.

---
+++ StringInt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+            int Edad;
+            Console.WriteLine(" Escriba su nombre: ");
+            Nombre = Console. ReadLine ();
+            Console.WriteLine(" \n Ingrese su edad: ");
+            Edad = int.Parse(Console.ReadLine());
+            Console.WriteLine(" \n Tu Nombre es: " + Nombre + " y tienes: " + Edad + " años ");
+            Console.WriteLine(" \n Presiona una tecla para finalizar ");
+            Console.ReadKey();
+        }
+    }
+}
